d868b359b3aefd435cdcfcb0134c9169a573bc72
pxpie <pxpietrzak@outlook.com>
2020-04-18 20:34:25 +0200

    Start the project and add new page selected by button clicked

---
+++ XamarinFormsDemo/XamarinFormsDemo/Covid/Covid19PageView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsDemo.Covid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Covid19PageView : ContentPage
+    {
+        public Covid19PageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
